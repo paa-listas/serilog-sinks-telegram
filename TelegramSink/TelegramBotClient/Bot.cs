90ea22a54aec7f44d7a640c73c4dd28e89a5a97d
4 <4@everywhere.com.ar>
2023-10-25 16:10:30 -0300

    Se implementa el Sink usando RestSharp en lugar de Flurl. En mis proyectos uso RestSharp asi que es mas conveniente para mi.
    Se actualizan los TargetFramework de ambos proyectos.

--- TelegramSink/TelegramBotClient/Bot.cs
+++ TelegramSink/TelegramBotClient/Bot.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Flurl;
-using Flurl.Http;
+using RestSharp;
 using TelegramSink.TelegramBotClient.Domain;
 
 namespace TelegramSink.TelegramBotClient
@@ -12,25 +9,37 @@ namespace TelegramSink.TelegramBotClient
         private readonly BotConfiguration _botConfiguration;
         private const string TelegramApiBaseUrl = "https://api.telegram.org";
 
+        private RestClient m_restClient = null;
+        private string m_toBoot = string.Empty;
+
         public Bot(BotConfiguration botConfiguration)
         {
             _botConfiguration = botConfiguration;
+
+            // Opciones de configuracion
+            Uri uri = new Uri(TelegramApiBaseUrl);
+            RestClientOptions options = new RestClientOptions(uri) { MaxTimeout = 5000 };
+            string userAgent = "TelergamSink/0.1.2 (RestSharp; . Net Standard 2.1; rv:0) 20221010";
+            options.UserAgent = userAgent;
+
+            // Creamos el cliente, solo una instancia hace falta.
+            m_restClient = new RestClient(options);
+
+            // Debemos encodear el : del ApiKey - No debe empezar con '/bot' es solo 'bot'
+            m_toBoot = string.Format("bot{0}/sendMessage", System.Net.WebUtility.UrlEncode(_botConfiguration.ApiKey));
         }
 
-        public RestResult SendMessage(string message)
-        {
-            var response = TelegramApiBaseUrl
-                .AppendPathSegment($"bot{_botConfiguration.ApiKey}/sendMessage")
-                .PostJsonAsync(new { chat_id=_botConfiguration.ChatId, text=message })
-                .ReceiveJson<RestResult>().Result;
 
-            return response;
+        public ResultMessage SendMessage(string message, string parse_mode = "Markdown")
+        {
+            var request = new RestRequest(m_toBoot, Method.Get);
+            request.AddQueryParameter("parse_mode", parse_mode); // HTML or Markdown or MarkdownV2 or omitir este parametro.
+            request.AddQueryParameter("chat_id", _botConfiguration.ChatId);
+            request.AddQueryParameter("text", message);
+
+            var response = m_restClient.Execute<SendMessageResponse>(request);
+
+            return response.Data.ResultMessage;
         }
     }
-
-    public class RestResult
-    {
-		public bool Ok { get; set; }
-        public Message Result { get; set; }
-	}
 }
--- TelegramSink/TelegramBotClient/Domain/Message.cs
+++ TelegramSink/TelegramBotClient/Domain/Message.cs
@@ -1,12 +1,73 @@
-﻿using Newtonsoft.Json;
+﻿using System.Text.Json.Serialization;
 
 namespace TelegramSink.TelegramBotClient.Domain
 {
-	public class Message
-	{
-		[JsonProperty(PropertyName = "message_id")]
-		public string MessageId { get; set; }
-		[JsonProperty(PropertyName = "date")]
-		public long Date { get; set; }
-	}
+    public class Chat
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("last_name")]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("username")]
+        public string Username { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+    }
+
+    /// <summary>
+    /// Informacion referente al bot con el cual estamos
+    /// interactuando.
+    /// </summary>
+    public class From
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("is_bot")]
+        public bool IsBot { get; set; }
+
+        [JsonPropertyName("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("username")]
+        public string Username { get; set; }
+    }
+
+    public class ResultMessage
+    {
+        [JsonPropertyName("message_id")]
+        public int MessageId { get; set; }
+
+        [JsonPropertyName("from")]
+        public From From { get; set; }
+
+        [JsonPropertyName("chat")]
+        public Chat Chat { get; set; }
+
+        [JsonPropertyName("date")]
+        public int Date { get; set; }
+
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+    }
+
+    /// <summary>
+    /// Respuesta recibida al enviar un mensaje a https://api.telegram.org/botXXXX/sendMessage?
+    /// </summary>
+    public class SendMessageResponse
+    {
+        [JsonPropertyName("ok")]
+        public bool Ok { get; set; }
+
+        [JsonPropertyName("result")]
+        public ResultMessage ResultMessage { get; set; }
+    }
+
+
 }
